d285887f80bec6d0708836e514085d81f4251358
Kaua Mauricio <kauacariocaktd@gmail.com>
2022-04-07 12:18:21 -0300

    subindo pastas de manipulacao de dados

---
+++ manipulacao_de_data/getters.js
@@ -0,0 +1,16 @@
+// 2022-03-09T11:33:27.961Z
+const dataAtual = new Date();
+
+
+console.log( dataAtual.getTime() );
+console.log( dataAtual.getTimezoneOffset() );
+console.log( dataAtual.valueOf() );
+
+console.log( dataAtual.getUTCFullYear() );
+console.log( dataAtual.getUTCMonth() );
+console.log( dataAtual.getUTCDate() );
+console.log( dataAtual.getUTCHours() );
+console.log( dataAtual.getUTCMinutes() );
+console.log( dataAtual.getUTCSeconds() );
+console.log( dataAtual.getUTCMilliseconds() );
+console.log( dataAtual.getUTCDay() );
